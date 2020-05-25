6383d930af625e8405fe562e9d5b9af1bdc86dd8
Citillara <Citillara@users.noreply.github.com>
2020-05-25 20:47:49 +0200

    Cleaning

--- main.lua
+++ main.lua
@@ -5,73 +5,33 @@ local damageEvents = {
     SPELL_DAMAGE = true,
 }
 
-local f = CreateFrame("Frame")
-f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-f:SetScript("OnEvent", function(self, event)
-    self:OnEvent(event, CombatLogGetCurrentEventInfo())
-end)
-
 local score = 0
 local runallowed = 1
 local visible = 1
-
+local debugprint = 0
 
 local frame = CreateFrame("FRAME"); -- Need a frame to respond to events
 frame:RegisterEvent("ADDON_LOADED"); -- Fired when saved variables are loaded
 frame:RegisterEvent("PLAYER_LOGOUT"); -- Fired when about to log out
 
-
-
-local function isempty(s)
-  return s == nil or s == ''
-end
-
-
-function frame:OnEvent(event, arg1)
- if event == "ADDON_LOADED" then
-  -- Our saved variables are ready at this point. If there are none, both variables will set to nil.
-    if isempty(NepNepScore) then
-        NepNepScore = score
-    else
-        score = NepNepScore
-    end
-
-    if isempty(NepNepEnabled) then
-        NepNepEnabled = runallowed
-    else
-        runallowed = NepNepEnabled
-    end
-
-    if isempty(NepNepVisible) then
-        NepNepVisible = visible
-    else
-        visible = NepNepVisible
-    end
-
- elseif event == "PLAYER_LOGOUT" then
-    NepNepScore = score
-    NepNepEnabled = runallowed
-    NepNepVisible = visible
- end
-end
-
 frame:SetScript("OnEvent", frame.OnEvent);
 
 
-SLASH_HAVEWEMET1 = "/hwm";
-function SlashCmdList.HAVEWEMET(msg)
- print("HaveWeMet has met " .. HaveWeMetCount .. " characters.");
-end
+local combatframe = CreateFrame("FRAME"); -- Need a frame to respond to events
+combatframe:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+combatframe:SetScript("OnEvent", function(self, event)
+    self:OnEvent(event, CombatLogGetCurrentEventInfo())
+end)
 
 SLASH_NEPNEPSCORE1 = "/nepnepscore"
-SLASH_NEPNEPSCORE2 = "/nns"
+SLASH_NEPNEPSCORE2 = "/nnscore"
 SlashCmdList["NEPNEPSCORE"] = function(message)
     print(score)
 end
 
 
 SLASH_NEPNEPRESET1 = "/nepnepreset"
-SLASH_NEPNEPRESET2 = "/nnr"
+SLASH_NEPNEPRESET2 = "/nnreset"
 SlashCmdList["NEPNEPRESET"] = function(message)
     score = 0
     NepNepScore = 0
@@ -106,16 +66,133 @@ SlashCmdList["NEPNEPHIDE"] = function(message)
     NepNep:Hide()
 end
 
-print("NepNep  Loaded")
+SLASH_NEPNEPPRINTDEBUG1 = "/nepnepprintdebug"
+SLASH_NEPNEPPRINTDEBUG2 = "/nndbgon"
+SlashCmdList["NEPNEPPRINTDEBUG"] = function(message)
+    debugprint = 1
+    print("NepNep printing debug")
+end
+
+SLASH_NEPNEPSTOPPRINTDEBUG1 = "/nepnepstopprintdebug"
+SLASH_NEPNEPSTOPPRINTDEBUG2 = "/nndbgoff"
+SlashCmdList["NEPNEPSTOPPRINTDEBUG"] = function(message)
+    debugprint = 0
+    print("NepNep stopped printing debug")
+end
+
+
+local t = {}
+
+local function isempty(s)
+  return s == nil or s == ''
+end
+
+
+local function C(v)
+    if v == nil then
+        return "nil"
+    else
+        return v
+    end
+end
+
+local function dprint(msg)
+    if debugprint == 1 then
+        print(msg)
+    end
+end
+
+
+
+local LARGE_NUMBER_SEPERATOR, DECIMAL_SEPERATOR = LARGE_NUMBER_SEPERATOR, DECIMAL_SEPERATOR
+local function SeparateDigits(number, thousands, decimal)
+    local symbol
+    if type(number) == "string" then
+        local value
+        value, symbol = number:match("^([-%d.]+)(.*)")
+        if not value then
+            return number
+        end
+        number = tonumber(value)
+    end
+    local int = math.abs(math.floor(number))
+    local rest = tostring(number):match("^[-%d.]+%.(%d+)") -- fuck off precision errors
+    if number < 0 then
+        t[#t+1] = "-"
+    end
+    if int < 1000 then
+        t[#t+1] = int
+    else
+        local digits = math.log10(int)
+        local segments = math.floor(digits / 3)
+        t[#t+1] = math.floor(int / 1000^segments)
+        for i = segments-1, 0, -1 do
+            t[#t+1] = thousands or LARGE_NUMBER_SEPERATOR
+            t[#t+1] = ("%03d"):format(math.floor(int / 1000^i) % 1000)
+        end
+    end
+    if rest then
+        t[#t+1] = decimal or DECIMAL_SEPERATOR
+        t[#t+1] = rest
+    end
+    if symbol then
+        t[#t+1] = symbol
+    end
+    local s = table.concat(t)
+    wipe(t)
+    return s
+end
+
 
 function NepNep_OnUpdate(self, elapsed)
-    Meow:SetText(score)
+    if isempty(score) then
+        return
+    end
+    Meow:SetText(SeparateDigits(score," ",  ","))
 end
 
 function NepNep_OnMouseDown(self, button)
 end
 
-function f:OnEvent(event, ...)
+function frame:OnEvent(event, arg1)
+ if event == "ADDON_LOADED" then
+  -- Our saved variables are ready at this point. If there are none, both variables will set to nil.
+    print("NepNep ADDON_LOADED")
+
+    if isempty(NepNepScore) then
+        NepNepScore = score
+    else
+        score = NepNepScore
+    end
+
+    if isempty(NepNepEnabled) then
+        NepNepEnabled = runallowed
+    else
+        runallowed = NepNepEnabled
+    end
+
+    if isempty(NepNepVisible) then
+        NepNepVisible = visible
+    else
+        visible = NepNepVisible
+    end
+
+    if isempty(NepNepDebugPrint) then
+        NepNepDebugPrint = debugprint
+    else
+        debugprint = NepNepDebugPrint
+    end
+
+
+ elseif event == "PLAYER_LOGOUT" then
+    NepNepScore = score
+    NepNepEnabled = runallowed
+    NepNepVisible = visible
+ end
+end
+
+
+function combatframe:OnEvent(event, ...)
     local timestamp, event, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, extraArg1, extraArg2, extraArg3, extraArg4, extraArg5, extraArg6, extraArg7, extraArg8, extraArg9, extraArg10 = CombatLogGetCurrentEventInfo()
 
     if runallowed == 0 then
@@ -125,17 +202,17 @@ function f:OnEvent(event, ...)
     local amount = 0
 
 
-    if sourceGUID == playerGUID or destGUID == playerGUID  then
-        print("Event " .. event)
-    end
+    --if sourceGUID == playerGUID or destGUID == playerGUID  then
+    --    print("Event " .. event)
+    --end
 
     if sourceGUID == playerGUID and damageEvents[event] then
         if event == "SWING_DAMAGE" then
-            print("Player swing dealt " .. extraArg1 .. " with " .. extraArg2 .. " overkill")
+            dprint("[" .. timestamp .."] " .. "Player swing dealt " .. extraArg1 .. " with " .. extraArg2 .. " overkill")
             amount = extraArg1 - extraArg2
         end
         if event == "SPELL_DAMAGE" then
-            print("Player spell dealt " .. extraArg4 .. " with " .. extraArg5 .. " overkill")
+            dprint("[" .. timestamp .."] " .. "Player spell dealt " .. extraArg4 .. " with " .. extraArg5 .. " overkill")
             amount = extraArg4 - extraArg5
         end
         score = score + amount
@@ -144,39 +221,48 @@ function f:OnEvent(event, ...)
 
     if destGUID == playerGUID and damageEvents[event] then
         if event == "SWING_DAMAGE" then
-            print("Ennemy swing dealt " .. extraArg1 .. " with " .. extraArg2 .. " overkill")
+            dprint("[" .. timestamp .."] " .. "Ennemy swing dealt " .. extraArg1 .. " with " .. extraArg2 .. " overkill")
+            amount = extraArg1 - extraArg2
         end
         if event == "SPELL_DAMAGE" then
-            print("Ennemy spell dealt " .. extraArg4 .. " with " .. extraArg5 .. " overkill")
+            dprint("[" .. timestamp .."] " .. "Ennemy spell dealt " .. extraArg4 .. " with " .. extraArg5 .. " overkill")
+            amount = extraArg4 - extraArg5
         end
+        score = score - amount
     end
 
 
-
     if destGUID == playerGUID and event == "SPELL_HEAL" then
-
         if sourceGUID == playerGUID then
-            print("Player self healed for " .. extraArg4 .. " with " .. extraArg5 .. " overheal" )
+            dprint("[" .. timestamp .."] " .. "Player self healed for " .. extraArg4 .. " with " .. extraArg5 .. " overheal" )
+            amount = extraArg4 - extraArg5
         else
-            print(sourceName .." healed player for " .. extraArg4 .. " with " .. extraArg5 .. " overheal" )
+            dprint("[" .. timestamp .."] " .. sourceName .." healed player for " .. extraArg4 .. " with " .. extraArg5 .. " overheal" )
+            amount = 0 - extraArg4 - extraArg5
         end
+        score = score + amount
     end
 
     if event == "PARTY_KILL" then
         if sourceGUID == playerGUID then
-            print("Player killed " .. C(destName))
+            dprint("[" .. timestamp .."] " .. "Player killed " .. C(destName))
+            score = score + 1000
         else
-            print(C(sourceName) .. " killed " .. C(destName))
+            dprint("[" .. timestamp .."] " .. C(sourceName) .. " killed " .. C(destName))
+            score = score - 500
         end
     end
 
     -- extraArg1 = Falling
     if destGUID == playerGUID and event == "ENVIRONMENTAL_DAMAGE" then
-        print("ENVIRONMENT damaged " .. C(destName) .. " for " .. C(extraArg2)) 
+        dprint("[" .. timestamp .."] " .. "ENVIRONMENT damaged " .. C(destName) .. " for " .. C(extraArg2))
+        amount = extraArg2
+        score = score - amount
     end
 
     if destGUID == playerGUID and event == "UNIT_DIED" then
-        print("Player died")
+        dprint("[" .. timestamp .."] " .. "Player died")
+        score = score - 1000000
     end
 
 
@@ -195,16 +281,7 @@ if event == "SPELL_AURA_APPLIED" then
     end
     ]]--
 
-    if amount ~= 0 then
-        NepNepScore = score
-    end
 
 end
 
-function C(v)
-    if v == nil then
-        return "nil"
-    else
-        return v
-    end
-end
+print("NepNep Loaded")
